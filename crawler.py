96d493c709bd2749caa61b83e7285125479ea0ce
suarezmanuel <manuelsuarez244@gmail.com>
2025-09-01 14:19:37 +0300

    searching to behold the stories that are told when my back is to the world

---
+++ crawler.py
@@ -0,0 +1,75 @@
+import requests
+import time 
+
+START_ID = 300000
+RANGE = 1000
+OUTPUT_FILE = "video_ids.txt"
+URL_TEMPLATE = "https://lemida.biu.ac.il/local/video_directory/thumb.php?id={}"
+
+session = requests.Session()
+session.headers.update({'User-Agent': 'Mozilla/5.0'})
+
+def check_status_code(video_id):
+    check_url = URL_TEMPLATE.format(video_id)
+    try: 
+        response = session.head(check_url, allow_redirects=True, timeout=10)
+        return response.status_code
+    except requests.exceptions.RequestException as e:
+        pass
+    return None
+
+plot_info = []
+
+def crawl():
+    total_found = 0
+    last_time = time.time()
+    last_id = -1
+    
+    try:
+        with open('files/log.txt', 'w') as f, open('files/results.txt', 'a') as g, open('files/last_id.txt', 'r') as h:
+            # so we dont repeat id's, continue from last one. 
+            # assuming we will always start from START_ID for now
+            line = h.readline()
+            if len(line):
+                # upgrade this to use 'seek', if file is big it will be problematic.
+                first_id = int(line)+1
+            else: 
+                first_id = START_ID
+                
+            print(first_id)
+                
+            g = open('files/results.txt', 'a')
+            last_id = first_id
+
+            for video_id in range(first_id, first_id+RANGE):
+                
+                curr_time = time.time()
+                time_taken = curr_time-last_time
+                
+                status_code = check_status_code(video_id)
+                string = f'{video_id}: {status_code}. time taken : {time_taken}\n'
+                
+                last_time = curr_time
+                
+                f.write(string)
+                print(string)
+
+                if (time_taken >= 1):
+                    print("too much time passed, timeout?")
+                    f.write("too much time passed, timeout?\n")
+                    break
+                if (status_code not in [200, 404]):
+                    print("new status code")
+                    f.write("new status code\n")
+                    break
+                if (status_code == 200):
+                    g.write(f'found thumbnail: {video_id}\n')
+                    total_found += 1
+                    plot_info.append({'video_id': video_id, 'time_taken': time_taken})
+                    last_id = video_id
+
+        print(f'found a total of: {total_found} videos')
+    finally:
+        with open('files/last_id.txt', 'w') as h:
+            if last_id == -1: print("last_id is -1 : error")
+            h.write(str(last_id))
---
+++ crawler_server.py
@@ -0,0 +1,32 @@
+import time
+import json
+import threading
+from flask import Flask, render_template, jsonify
+from crawler import plot_info as plot_data, crawl
+
+# --- Global data storage (shared between threads) ---
+# Use a lock to prevent race conditions when updating the list
+data_lock = threading.Lock()
+
+# --- Flask Web Server ---
+app = Flask(__name__)
+
+# This route serves the HTML page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# This route provides the data to the chart
+@app.route('/data')
+def get_data():
+    with data_lock:
+        return jsonify(plot_data)
+
+if __name__ == "__main__":
+    # Start the crawler in a separate thread
+    crawler_thread = threading.Thread(target=crawl, daemon=True)
+    crawler_thread.start()
+    
+    # Run the Flask web server
+    # use_reloader=False is important to prevent running the crawler twice
+    app.run(debug=True, use_reloader=False)
